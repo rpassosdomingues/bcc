9fd381b952d1d244d7c7f0cae29c867443e40ea3
Rafael Passos Domingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-05-19 09:14:08 -0300

    may fri 19

--- dev-cpp/aeds1/lista01_pratica/problem4.cpp
+++ dev-cpp/aeds1/lista01_pratica/problem4.cpp
@@ -1,32 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int m, n;
-    printf("Digite o valor de m: ");
+    
+    int m, n, x, y, expressao, valorMax, xMax, yMax;
+
+    x = 1;
+    y = 1;
+    valorMax = 0;
+
+    printf("\n Digite o valor de m: ");
     scanf("%d", &m);
-    printf("Digite o valor de n: ");
+    printf("\n Digite o valor de n: ");
     scanf("%d", &n);
 
-    int max_value = 0;
-    int max_x, max_y;
-
-    int x = 1;
     while (x < m) {
-        int y = 1;
-        while (y < n) {
-            int value = x * y - x * x + y;
-            if (value > max_value) {
-                max_value = value;
-                max_x = x;
-                max_y = y;
-            }
-            y++;
-        }
-        x++;
+      while (y < n) {
+        expressao = x*y - x*x + y;
+          if (expressao > valorMax) {
+            valorMax = expressao;
+            xMax = x;
+            yMax = y;
+          }
+          y++;
+      }
+      x++;
     }
 
-    printf("O par (x, y) que maximiza a expressão é: (%d, %d)\n", max_x, max_y);
-    printf("O valor máximo encontrado é: %d\n", max_value);
+    printf("\n\t O par (x, y) que maximiza a expressão é: (%d, %d)\n", xMax, yMax);
+    printf("\t O valor máximo encontrado é: %d \n\n", valorMax);
 
     return 0;
 }
