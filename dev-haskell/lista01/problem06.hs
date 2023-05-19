d32329d52cd05f1a286aa551c13642cdf1074771
Rafael Passos Domingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-05-19 09:24:02 -0300

    may fri 19

--- dev-haskell/lista01/problem06.hs
+++ dev-haskell/lista01/problem06.hs
@@ -1,12 +1,3 @@
 arvore :: Float -> Float
---x = 6
---i0 = sqrt x
-
--- i1 = sqrt ( x + i0 )
--- i2 = sqrt ( x + i1 )
--- i3 = sqrt ( x + i2 )
--- i4 = sqrt ( x + i3 )
--- in = sqrt ( x + in-1 )
-
-arvore x = sqrt (x + sqrt (x-1))
-
+arvore 0 = 0
+arvore x = sqrt (x + arvore (x-1))
