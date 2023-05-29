86a23bbb8aab05b57ad48bddab49c1327cdc027d
Rafael Passos Domingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-05-29 08:33:42 -0300

    may mon 29

--- dev-cpp/aeds1/aulas/BD_veiculos.cpp
+++ dev-cpp/aeds1/aulas/BD_veiculos.cpp
@@ -21,8 +21,8 @@ Após o final de leitura do arquivo, o projeto deve apresentar de forma clara as
 using namespace std;
 
 int ano, quilometragem;
-string placa, cor, direcao, trasmissao, tipo, marca;
 float preco, motor;
+string placa, cor, direcao, trasmissao, tipo, marca;
 
 int main() {
     // Definir o número total de veículos e contadores para as categorias
@@ -95,7 +95,7 @@ int main() {
 
         // Verificar se o veículo tem 5 anos ou mais (2018)
         if (ano <= 2018) {
-            somaKilometragem5AnosMais += quilometragem;
+            somaKM5AnosMais += quilometragem;
         }
 
         totalVeiculos++;
@@ -112,23 +112,48 @@ int main() {
     float percentualAutoHidraulica = (float) (veiculosAutoHidraulica / totalVeiculos) * 100;
 
     // Calcular o valor da prestação do financiamento
-    float taxaJuros = 0.8; // Exemplo: 0.8% ao mês
-    int numParcelas = 48;
-    float prestacao = (valor * taxaJuros) / (1 - pow(1 + taxaJuros, -numParcelas)); 
+    float taxaJurosHatch = 0.8;
+    int numParcelasHatch = 48;
+    float prestacaoHatch = (valorHatch * taxaJurosHatch) / (1 - pow(1 + taxaJurosHatch, -numParcelasHatch));
+
+    float taxaJurosSUV = 0.8;
+    int numParcelasSUV = 48;
+    float prestacaoSUV = (valorSUV * taxaJurosSUV) / (1 - pow(1 + taxaJurosSUV, -numParcelasSUV));
+
+    float taxaJurosSedan = 0.8;
+    int numParcelasSedan = 48;
+    float prestacaoSedan = (valorSedan * taxaJurosSedan) / (1 - pow(1 + taxaJurosSedan, -numParcelasSedan));
+
+    float taxaJurosPickup = 0.8;
+    int numParcelasPickup = 48;
+    float prestacaoPickup = (valorPickup * taxaJurosPickup) / (1 - pow(1 + taxaJurosPickup, -numParcelasPickup));
+
+    float taxaJurosVan = 0.8;
+    int numParcelasVan = 48;
+    float prestacaoVan = (valorVan * taxaJurosVan) / (1 - pow(1 + taxaJurosVan, -numParcelasVan));
+
+    float taxaJurosPasseio = 0.8;
+    int numParcelasPasseio = 48;
+    float prestacaoPasseio = (valorPasseio * taxaJurosPasseio) / (1 - pow(1 + taxaJurosPasseio, -numParcelasPasseio));
 
     // Calcular o seguro estimado (10% do valor do veículo)
-    seguroEstimado = precoMaximo * 0.1;
+    seguroEstimadoHatch   = precoMaximoHatch   * 0.1;
+    seguroEstimadoSUV     = precoMaximoSUV     * 0.1;
+    seguroEstimadoSedan   = precoMaximoSedan   * 0.1;
+    seguroEstimadoPickup  = precoMaximoPickup  * 0.1;
+    seguroEstimadoVan     = precoMaximoVan     * 0.1;
+    seguroEstimadoPasseio = precoMaximoPasseio * 0.1;
 
     // Calcular a média de quilometragem dos veículos com 5 anos ou mais
-    float mediaKilometragem5AnosMais = (float)somaKilometragem5AnosMais / veiculos5AnosMais;
+    float mediaKM5AnosMais = (float)somaKM5AnosMais / veiculos5AnosMais;
 
     // Imprimir os resultados
     printf("\n\n Porcentagens de veículos nas categorias de tipo:\n");
-    printf("\t Hatch: %.2f%% \n", percentualHatch);
-    printf("\t SUV: %.2f%% \n", percentualSUV);
-    printf("\t Sedan: %.2f%% \n", percentualSedan);
+    printf("\t   Hatch: %.2f%% \n", percentualHatch);
+    printf("\t     SUV: %.2f%% \n", percentualSUV);
+    printf("\t   Sedan: %.2f%% \n", percentualSedan);
     printf("\t Pick-up: %.2f%% \n", percentualPickup);
-    printf("\t Van: %.2f%% \n", percentualVan);
+    printf("\t     Van: %.2f%% \n", percentualVan);
     printf("\t Passeio: %.2f%% \n", percentualPasseio);
 
     printf("\n %.2f%% dos veículos possuem câmbio automático e direção hidráulica \n", percentualAutoHidraulica);
@@ -145,7 +170,7 @@ int main() {
 
     printf("\n Quantidade e média de quilometragem dos veículos com 5 anos ou mais: \n");
     printf("\t Quantidade: %d \n", veiculos5AnosMais);
-    printf("\t Média de quilometragem: %.2f \n\n", mediaKilometragem5AnosMais);
+    printf("\t Média de quilometragem: %.2f \n\n", mediaKM5AnosMais);
 
     // Fechar o arquivo
     teclado.close();
