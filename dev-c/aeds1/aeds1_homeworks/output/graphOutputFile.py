50d32befbf46a32146dcde42f04d0ff5046788ee
Rafael Passos Domingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-06-27 21:30:37 -0300

    jun 27 tue

--- dev-c/aeds1/aeds1_homeworks/output/graphOutputFile.py
+++ dev-c/aeds1/aeds1_homeworks/output/graphOutputFile.py
@@ -1,47 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Carregar dados do arquivo CSV
-data = pd.read_csv('sort_usages.csv')
+# Ler o arquivo CSV
+data = pd.read_csv("sort_usages.csv")
 
-# Extrair colunas relevantes
-sizes = data['Array Size']
-randomBubbleUsages = data['Random Bubble Sort Usage']
-increasingBubbleUsages = data['Increasing Bubble Sort Usage']
-decreasingBubbleUsages = data['Decreasing Bubble Sort Usage']
-randomInsertionUsages = data['Random Insertion Sort Usage']
-increasingInsertionUsages = data['Increasing Insertion Sort Usage']
-decreasingInsertionUsages = data['Decreasing Insertion Sort Usage']
-randomSelectionUsages = data['Random Selection Sort Usage']
-increasingSelectionUsages = data['Increasing Selection Sort Usage']
-decreasingSelectionUsages = data['Decreasing Selection Sort Usage']
+# Extrair os dados das colunas
+sizes = data["Array Size"]
+randomBubbleUsages = data["Random Bubble Sort Usage"]
+increasingBubbleUsages = data["Increasing Bubble Sort Usage"]
+decreasingBubbleUsages = data["Decreasing Bubble Sort Usage"]
+randomInsertionUsages = data["Random Insertion Sort Usage"]
+increasingInsertionUsages = data["Increasing Insertion Sort Usage"]
+decreasingInsertionUsages = data["Decreasing Insertion Sort Usage"]
+randomSelectionUsages = data["Random Selection Sort Usage"]
+increasingSelectionUsages = data["Increasing Selection Sort Usage"]
+decreasingSelectionUsages = data["Decreasing Selection Sort Usage"]
 
-# Plotar gráfico de dispersão para Bubble Sort
-plt.scatter(sizes, randomBubbleUsages, label='Random Bubble Sort')
-plt.scatter(sizes, increasingBubbleUsages, label='Increasing Bubble Sort')
-plt.scatter(sizes, decreasingBubbleUsages, label='Decreasing Bubble Sort')
-plt.xlabel('Array Size')
-plt.ylabel('Number of Usages')
-plt.title('Bubble Sort Usages')
+# Plotar os gráficos
+plt.figure(figsize=(10, 6))
+
+# Gráfico Bubble Sort
+plt.subplot(2, 1, 1)
+plt.plot(sizes, randomBubbleUsages, label="Random")
+plt.plot(sizes, increasingBubbleUsages, label="Increasing")
+plt.plot(sizes, decreasingBubbleUsages, label="Decreasing")
+plt.title("Bubble Sort")
+plt.xlabel("Array Size")
+plt.ylabel("Number of Iterations")
 plt.legend()
-plt.show()
 
-# Plotar gráfico de dispersão para Insertion Sort
-plt.scatter(sizes, randomInsertionUsages, label='Random Insertion Sort')
-plt.scatter(sizes, increasingInsertionUsages, label='Increasing Insertion Sort')
-plt.scatter(sizes, decreasingInsertionUsages, label='Decreasing Insertion Sort')
-plt.xlabel('Array Size')
-plt.ylabel('Number of Usages')
-plt.title('Insertion Sort Usages')
+# Gráfico Insertion Sort
+plt.subplot(2, 1, 2)
+plt.plot(sizes, randomInsertionUsages, label="Random")
+plt.plot(sizes, increasingInsertionUsages, label="Increasing")
+plt.plot(sizes, decreasingInsertionUsages, label="Decreasing")
+plt.title("Insertion Sort")
+plt.xlabel("Array Size")
+plt.ylabel("Number of Iterations")
 plt.legend()
-plt.show()
 
-# Plotar gráfico de dispersão para Selection Sort
-plt.scatter(sizes, randomSelectionUsages, label='Random Selection Sort')
-plt.scatter(sizes, increasingSelectionUsages, label='Increasing Selection Sort')
-plt.scatter(sizes, decreasingSelectionUsages, label='Decreasing Selection Sort')
-plt.xlabel('Array Size')
-plt.ylabel('Number of Usages')
-plt.title('Selection Sort Usages')
-plt.legend()
+# Ajustar espaçamento entre os subplots
+plt.tight_layout()
+
+# Exibir os gráficos
 plt.show()
