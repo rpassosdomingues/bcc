a3a55e14123e0cbdaa35dd3896535355c19687dd
Rafael Passos Domingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-06-28 12:11:10 -0300

    jun 28 wed

--- dev-python/graduacao-fisica/large-scale-galaxies-structure.py
+++ dev-python/graduacao-fisica/large-scale-galaxies-structure.py
@@ -1,45 +1,39 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-class SimulatedDensityField:
-    def __init__(self, cdm_data, wdm_data):
-        self.cdm_data = cdm_data
-        self.wdm_data = wdm_data
-        self.fig, self.ax = plt.subplots(1, 2, figsize=(10, 5))
+def fractal_galaxies(x, y, size, angle, depth):
+    if depth == 0:
+        return
 
-    def plot_density_field(self):
-        # Left panel: Cold Dark Matter
-        ax1 = self.ax[0]
-        ax1.imshow(self.cdm_data, cmap='viridis', origin='lower')
-        ax1.set_title("Cold Dark Matter")
-        ax1.set_xlabel("X-axis")
-        ax1.set_ylabel("Y-axis")
-        ax1.text(0.5, 1.1, "Simulated Density Field of Cold Dark Matter", ha="center", va="center", fontsize=12)
-        ax1.text(0.5, 1.0, "Image adapted from [Reference]", ha="center", va="center", fontsize=10)
+    x_end = x + size * np.sin(np.deg2rad(angle))
+    y_end = y + size * np.cos(np.deg2rad(angle))
+    
+    # Plotar a galáxia
+    plt.scatter(x_end, y_end, color='black', s=size)
 
-        # Right panel: Warm Dark Matter
-        ax2 = self.ax[1]
-        ax2.imshow(self.wdm_data, cmap='viridis', origin='lower')
-        ax2.set_title("Warm Dark Matter")
-        ax2.set_xlabel("X-axis")
-        ax2.set_ylabel("Y-axis")
-        ax2.text(0.5, 1.1, "Simulated Density Field of Warm Dark Matter", ha="center", va="center", fontsize=12)
-        ax2.text(0.5, 1.0, "Image adapted from [Reference]", ha="center", va="center", fontsize=10)
+    # Calcular os novos parâmetros para as galáxias filhas
+    new_size = size * 0.8
+    left_angle = angle + 30
+    right_angle = angle - 30
 
-        plt.tight_layout()
-        plt.show()
+    # Chamada recursiva para as galáxias filhas
+    fractal_galaxies(x_end, y_end, new_size, left_angle, depth-1)
+    fractal_galaxies(x_end, y_end, new_size, right_angle, depth-1)
 
+# Configurações iniciais
+depth = 8  # Profundidade da árvore
+size = 100  # Tamanho inicial das galáxias
+angle = 90  # Ângulo inicial
 
-# Define the simulated density field data for Cold Dark Matter (cdm) and Warm Dark Matter (wdm)
-cdm_data = [[0.1, 0.2, 0.3],
-            [0.4, 0.5, 0.6],
-            [0.7, 0.8, 0.9]]
+# Criar a figura
+fig, ax = plt.subplots()
 
-wdm_data = [[0.9, 0.8, 0.7],
-            [0.6, 0.5, 0.4],
-            [0.3, 0.2, 0.1]]
+# Chamar a função fractal_galaxies para desenhar as galáxias
+fractal_galaxies(0, 0, size, angle, depth)
 
-# Create an instance of the SimulatedDensityField class
-sim_density_field = SimulatedDensityField(cdm_data, wdm_data)
+# Configurações do gráfico
+ax.set_aspect('equal', adjustable='box')
+ax.axis('off')
 
-# Plot the simulated density field of Cold Dark Matter and Warm Dark Matter
-sim_density_field.plot_density_field()
+# Mostrar o gráfico
+plt.show()
